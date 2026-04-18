58703f0c200f3932d0a8a610a6d2e70fd33c88ab
m-nsd <knsdgmknsd@gmail.com>
2026-04-19 08:38:43 +0900

    JavaScript11章まで済

--- js/gallery.js
+++ js/gallery.js
@@ -5,30 +5,31 @@ function activateGallery() {
   let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
   /* thumbnails選択の別の方法 11.2.1.演習2
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img"); */
-
   let mainImage = document.querySelector("#gallery-photo img");
-
-  //更新される画像情報
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
-
+  
   thumbnails.forEach(function(thumbnail) {
+    //　大画像をプリロードする
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
-      // クリックされたサムネイル画像をメイン画像として設定する
-      let newImageSrc = thumbnail.dataset.largeVersion;
+      //(1)クリックされたサムネイル画像をメイン画像として設定する
       mainImage.setAttribute("src", newImageSrc);
-      //メイン画像のalt属性も書き換える 11.2.1演習1
+      //(1)-2メイン画像のalt属性も書き換える 11.2.1演習1
       //サムネイルのaltを、メイン画像のaltに上書きする
       mainImage.setAttribute("alt", thumbnail.alt);
-      //alert(thumbnail.alt); altの変更確認
 
-      //サムネイル画像のオレンジ枠を、現在選択のサムネイルに変更する
+      //(2)サムネイル画像のオレンジ枠を、現在選択のサムネイル画像に変更する
       let currentClass = "current";//11.3.1演習
       document.querySelector(".current").classList.remove(currentClass);
       thumbnail.parentNode.classList.add(currentClass);
 
-      //画像情報を更新する
+      //(3)画像情報を更新する
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
       title.innerHTML = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
